226bc3d7a2687d57ee411f6fe3c9a15ab863f6e7
hmallen <allenhm@gmail.com>
2018-05-26 16:48:37 -0500

    Fixed variable names and moved to alpha5.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='chartgenerator',
-    version='0.1a4',
+    version='0.1a5',
     author='Hunter M. Allen',
     author_email='allenhm@gmail.com',
     license='MIT',
