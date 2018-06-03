4a0236bb8a244fbb5216aa034d4324548bf68f75
hmallen <allenhm@gmail.com>
2018-06-02 20:40:54 -0500

    Moved to alpha8.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='chartgenerator',
-    version='0.1a7',
+    version='0.1a8',
     author='Hunter M. Allen',
     author_email='allenhm@gmail.com',
     license='MIT',
